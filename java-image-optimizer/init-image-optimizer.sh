9bcc21382259b36c5d160f2a47ab04c93694e3ba
Yigal Nissan <ynissan@gmail.com>
2021-12-06 20:52:34 +0200

    installing maven

--- java-image-optimizer/init-image-optimizer.sh
+++ java-image-optimizer/init-image-optimizer.sh
@@ -4,7 +4,8 @@
 #docker image rm $(docker images)
 
 #docker build -t optimizer-image --build-arg IMAGE_IN=$1 --build-arg IMAGE_OUT=$2 .
-
+apt-get update
+apt-get install maven
 mvn clean compile
 #docker run -it --name optimizer-docker -d optimizer-image
 
