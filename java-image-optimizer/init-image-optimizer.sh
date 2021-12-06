bf4c3ed6eb2a44705acabe52b0cd3055a8d4d476
Yigal Nissan <ynissan@gmail.com>
2021-12-06 19:32:21 +0200

    working with maven

--- java-image-optimizer/init-image-optimizer.sh
+++ java-image-optimizer/init-image-optimizer.sh
@@ -3,8 +3,9 @@
 #docker container rm $(docker container ls -aq) -f
 #docker image rm $(docker images)
 
-docker build -t optimizer-image --build-arg IMAGE_IN=$1 --build-arg IMAGE_OUT=$2 .
+#docker build -t optimizer-image --build-arg IMAGE_IN=$1 --build-arg IMAGE_OUT=$2 .
 
+mvn clean compile
 #docker run -it --name optimizer-docker -d optimizer-image
 
 
